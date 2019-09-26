af61f7733c1f4aaf773bd771136a0e4828e2d5ef
ciro-souzagarcia <ciro@kayrus.com.br>
2019-09-26 14:57:48 +0000

    refatoração: remoção de funções globais

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-function chamarApi(){
-	// console.log("chamando API");
-	const endpoint = "api/latest";
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback);
+const ovly = {};
+
+ovly.conversor = {
+	versao: "1",
+	chamarApi: function(){
+		// console.log("chamando API");
+		const endpoint = "api/latest";
+		var oParametros = {
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback);
+	},
+	callback: function(resultado){
+		 console.log(resultado);
+		 
+		 var sMoedaDestino = $("#moeda_destino").val();
+		 var fValor = resultado.rates[sMoedaDestino];
+		$("#valor_destino2").text(fValor);
+		$("#moeda_destino2").text(sMoedaDestino);
+	}
 }
 
-function callback(resultado){
-	 console.log(resultado);
+// function chamarApi(){
+// 	// // console.log("chamando API");
+// 	// const endpoint = "api/latest";
+// 	// var oParametros = {
+// 	// 	base: $("#moeda_origem").val()
+// 	// };
+// 	// $.get(endpoint, oParametros, callback);
+// }
+
+// function callback(resultado){
+// 	//  console.log(resultado);
 	 
-	 var sMoedaDestino = $("#moeda_destino").val();
-	 var fValor = resultado.rates[sMoedaDestino];
-	$("#valor_destino2").text(fValor);
-	$("#moeda_destino2").text(sMoedaDestino);
-}
+// 	//  var sMoedaDestino = $("#moeda_destino").val();
+// 	//  var fValor = resultado.rates[sMoedaDestino];
+// 	// $("#valor_destino2").text(fValor);
+// 	// $("#moeda_destino2").text(sMoedaDestino);
+// }
